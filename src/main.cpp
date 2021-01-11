4b980c4e9e2df6b86aee363a424cb9a18ccbd572
SzymonGo1337 <46822941+SzymonGo1337@users.noreply.github.com>
2021-01-11 20:14:59 +0100

    rowan first version

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <fstream>
 
+std::string prefix = "[36m[ROWAN][0m ";
 std::string command, projname;
 
+const char* program = R"(#include <iostream>
+
+int main() {
+    std::cout << "Program\n";
+    std::cin.get();
+})";
+
 int main(int argc, char** argv) {
+    #ifdef _WIN32
+        system("Setlocal");
+    #endif
     for(int i = 0; i < argc; i++) {
         //std::cout << argv[i] << "\n";
         command = argv[i];
@@ -15,22 +26,36 @@ int main(int argc, char** argv) {
             std::string test2 = projname + "/.rowan";
             const char* data2 = test2.c_str();
             std::ofstream outFile(data2);
-            outFile << "g++ main.cpp -o " << projname;
+            outFile << "g++ " + projname + "/main.cpp -o " << projname;
             outFile.close();
-            std::cout << "Project with name: " << projname << " is initialized!\n";
+            std::string test3 = projname + "/main.cpp";
+            const char* data3 = test3.c_str();
+            std::ofstream outFile2(data3);
+            outFile2 << program;
+            outFile2.close();
+            std::cout << prefix << "Project with name: " << "[33m" << projname << "[0m" << " is initialized!\n";
         } else if(command == "--compile") {
             projname = argv[i + 1];
-            std::string test = ".rowan";
+            std::string test = projname + "/.rowan";
             const char* data = test.c_str();
             std::string out;
             std::ifstream inFile(data);
             while(std::getline(inFile, out)) {
-                std::cout << out << "\n";
+                std::cout << prefix << out << "\n";
             }
             inFile.close();
             const char* data2 = out.c_str();
             system(data2);
-            std::cout << "Project with name: " << projname << " is compiled!\n";
+        } else if(command == "--run") {
+            projname = argv[i + 1];
+            #ifdef _WIN32
+                std::string test = projname + ".exe";
+            #else
+                std::string test = "./" + projname;
+            #endif
+            const char* data = test.c_str();
+            system(data);
+            std::cout << prefix << "Program from project: " << "[33m" << projname << "[0m" << " is closed!\n";
         } else if(command == "--delete") {
             projname = argv[i + 1];
             #ifdef _WIN32
@@ -40,7 +65,7 @@ int main(int argc, char** argv) {
             #endif
             const char* data = test.c_str();
             system(data);
-            std::cout << "Project with name: " << projname << " is deleted!\n";
+            std::cout << prefix << "Project with name: " << "[33m" << projname << "[0m" << " is deleted!\n";
         }
     }
 }
